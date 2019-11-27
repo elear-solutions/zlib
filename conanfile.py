1cde07655521dd2730b4f3a08898cc4afb33b482
Rohan Sahay <rohansahay@elear.solutions>
2019-11-27 17:51:01 +0530

    fix conanfile

--- conanfile.py
+++ conanfile.py
@@ -1,30 +1,18 @@
 from conans import ConanFile, CMake, tools
 
 
-class ElearcommonlibConan(ConanFile):
+class ZlibConan(ConanFile):
     name = "zlib"
     version = "1.2.11"
     license = "<Put the package license here>"
     author = "<Put your name here> <And your email here>"
     url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Elearcommonlib here>"
+    description = "<Description of Zlib here>"
     topics = ("<Put some tag here>", "<here>", "<and here>")
     settings = "os", "compiler", "build_type", "arch"
-    #requires="json-c/1.13@demo/json-c"
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake"
-
-    #def source(self):
-    #    self.run("git clone https://github.com/memsharded/hello.git")
-    #    self.run("cd hello && git checkout static_shared")
-        # This small hack might be useful to guarantee proper /MT /MD linkage
-        # in MSVC if the packaged project doesn't have variables to set it
-        # properly
-    #    tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(MyHello)",
-    #                          '''PROJECT(MyHello)
-#include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-#conan_basic_setup()''')
 
     def build(self):
         cmake = CMake(self)
@@ -33,14 +21,8 @@ class ElearcommonlibConan(ConanFile):
         cmake.build()
         cmake.install()
 
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
-
     def package(self):
         self.copy("*.h", dst="include", src="package/include")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", src="package/lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
@@ -48,4 +30,3 @@ class ElearcommonlibConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = [ "z" ]
-
